a3039b1290fd736575cd9d9ca2c525eb7714b0cf
nicolaslee2000 <nicolaslee2000@gmail.com>
2022-03-28 14:49:27 +0900

    finished basic main feature to convert all files in a folder specified by extension

--- Converter/src/converter/Converter.java
+++ Converter/src/converter/Converter.java
@@ -17,7 +17,6 @@ import java.nio.charset.Charset;
 import java.nio.charset.CharsetDecoder;
 import java.util.HashSet;
 import java.util.Map;
-import java.util.Set;
 
 /**
  * @author Nicolaslee
@@ -26,37 +25,63 @@ import java.util.Set;
  */
 public class Converter {
 	
-	private File file;
+	private HashSet<File> files;
 	private Map<String, Charset> availableCharsets;
 	private Charset charset;
-	String charsetsToBeTested[];
+	private String charsetsToBeTested[];
+	private File root;
 	
-	Converter() {
+	private Converter() {
 		availableCharsets = Charset.availableCharsets();
-		charsetsToBeTested = new String[]{"UTF-8", "windows-1253", "ISO-8859-7", "euc-kr"};
-		
-	}
-	Converter(File file) {
-		this();
-		this.file = file;
+		files = new HashSet<>();
+		setCharsetsToBeTested(new String[]{"UTF-8", "windows-1253", "ISO-8859-7", "EUC-KR", "ISO-8859-1"});
 	}
 	
+	private void setCharsetsToBeTested(String[] charsetsToBeTested) {
+		this.charsetsToBeTested = charsetsToBeTested;
+	}
+
 	Converter(String filePath) {
 		this();
-		this.file = new File(filePath);
-	}
+		root = new File(filePath);
+		files.addAll(FileHandling.getAllFiles(root));
 	
-	public void changeExtension(String ext) {
-
-		int index = file.getAbsolutePath().lastIndexOf(".");
-		file.renameTo(new File(file.getAbsolutePath().substring(0, index + 1) + ext));
+		transform(files, new File("C:\\Users\\nicol\\Documents\\converting with java\\Testfolder - Copy"));
 	}
-	
-	public void transform() {
+		
+//	public void changeExtension(String ext) {
+//
+//		int index = file.getAbsolutePath().lastIndexOf(".");
+//		file.renameTo(new File(file.getAbsolutePath().substring(0, index + 1) + ext));
+//	}
 
+	
+	//if tgFolder is null automatically create copy of rootFolder to put converted files to. Otherwise put files to tgFolder
+	//use NIO to copy entire directory, possibly decoding while copying file(combine transform and convert)
+	//TODO ask if here is an easier way
+	public void transform(HashSet<File> files, File tgFolder) {
+		
+		//had to use NIO to get relative path
+		File folder = tgFolder == null ? new File(root.getParent(), root.getName()+" - Copy") : tgFolder;
+		folder.mkdir();
+		for(File file : files) {
+			File sf = new File(folder, root.toPath().relativize(file.toPath()).toString());
+			sf.getParentFile().mkdirs();
+			try {
+				sf.createNewFile();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			convert(file, sf, Charset.forName("euc-kr"), Charset.forName("utf-8"));
+		}
+		
+	}
+
+	
+	private void convert(File source, File target, Charset sourceCs, Charset targetCs) {
 		try (
-				BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(file),"euc-kr"));
-				BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(new File("C:\\Users\\nicol\\Documents\\converting with java\\plz.txt")),"Big5"));
+				BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(source), sourceCs));
+				BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(target), targetCs));
 				) {
 			int read;
 			while((read = reader.read()) != -1)
@@ -71,57 +96,56 @@ public class Converter {
 		}
 	}
 	
-	public Charset detectFileEncoding() {
-		CharsetDecoder decoder;
-		byte[] byteArray;
-		try (BufferedInputStream input = new BufferedInputStream(new FileInputStream(file));){
-			byteArray = new byte[500];
-			identified:
-			while ((input.read(byteArray) != -1)) {
-				for(String cs : charsetsToBeTested) {
-					decoder = Charset.forName(cs).newDecoder();
-					decoder.reset();
-					try {
-						decoder.decode(ByteBuffer.wrap(byteArray));
-					} catch (CharacterCodingException e) {
-						continue;
-					}
-					charset = Charset.forName(cs);
-					break identified;
-				}
-	        }
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return charset;
-	}
+//	public Charset detectFileEncoding() {
+//		CharsetDecoder decoder;
+//		byte[] byteArray;
+//		try (BufferedInputStream input = new BufferedInputStream(new FileInputStream(file));){
+//			byteArray = new byte[500];
+//			identified:
+//			while ((input.read(byteArray) != -1)) {
+//				for(String cs : charsetsToBeTested) {
+//					decoder = Charset.forName(cs).newDecoder();
+//					decoder.reset();
+//					try {
+//						decoder.decode(ByteBuffer.wrap(byteArray));
+//					} catch (CharacterCodingException e) {
+//						continue;
+//					}
+//					charset = Charset.forName(cs);
+//					break identified;
+//				}
+//	        }
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		}
+//		return charset;
+//	}
 	
 	//nested static class for readability. Use for getAllFiles methods only.
 	private static class FileHandling {
 		private FileHandling() {
 		}
 		
-		static HashSet<File> getAllFiles(File file) {
-			HashSet<File> listOfFiles = new HashSet<File>();
+		private static HashSet<File> getAllFiles(File file) {
+			HashSet<File> files = new HashSet<File>();
 	        if(file.isDirectory()) {
 	            for(File subFiles : file.listFiles()) {
 	                if(subFiles.isDirectory()) {
-	                    listOfFiles.addAll(getAllFiles(subFiles));
+	                    files.addAll(getAllFiles(subFiles));
 	                } else {
-	                    listOfFiles.add(subFiles);              
+	                    files.add(subFiles);              
 	                    }
 	            }
 	        } else {
-	            listOfFiles.add(file);              
+	            files.add(file);              
 	        }
-	        return listOfFiles;
+	        return files;
 	    }
 		private static HashSet<File> getAllFiles(File file, String... extension) {
 			HashSet<File> set = new HashSet<File>();
 			for(String ext : extension) {
 				for(File fl : getAllFiles(file)) {
-					int index = fl.getAbsolutePath().lastIndexOf(".");
-					if(fl.getAbsolutePath().substring(index+1).equals(ext)) {
+					if(fl.getAbsolutePath().toLowerCase().endsWith(ext.toLowerCase())) {
 						set.add(fl);
 					}
 				}
